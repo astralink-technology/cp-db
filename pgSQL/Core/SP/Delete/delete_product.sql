c1d947e6849459e15b5095d76b1130eb38a57cbe
shiweifong <shiweifong@gmail.com>
2014-04-08 19:14:14 +0800

    slight changes on db layer

--- pgSQL/Core/SP/Delete/delete_product.sql
+++ pgSQL/Core/SP/Delete/delete_product.sql
@@ -2,23 +2,23 @@
 DO $$
 DECLARE fname text;
 BEGIN
-FOR fname IN SELECT oid::regprocedure FROM pg_catalog.pg_proc WHERE proname = 'delete_product_registration' LOOP
+FOR fname IN SELECT oid::regprocedure FROM pg_catalog.pg_proc WHERE proname = 'delete_product' LOOP
   EXECUTE 'DROP FUNCTION ' || fname;
 END loop;
 RAISE INFO 'FUNCTION % DROPPED', fname;
 END$$;
 -- Start function
-CREATE FUNCTION delete_product_registration(
-        pProductRegistrationId varchar(32)
+CREATE FUNCTION delete_product(
+        pProductId varchar(32)
 )
 RETURNS BOOLEAN AS 
 $BODY$
 BEGIN
-    IF pProductRegistrationId IS NULL THEN  
+    IF pProductId IS NULL THEN  
         RETURN FALSE;
     ELSE
-        DELETE from product_registration where 
-        product_registration_id = pProductRegistrationId;
+        DELETE from product where 
+        product_id = pProductId;
         RETURN TRUE;
     END IF;
 END;
--- pgSQL/Core/SP/Delete/delete_product_registration.sql
+++ pgSQL/Core/SP/Delete/delete_product_registration.sql
@@ -2,23 +2,23 @@
 DO $$
 DECLARE fname text;
 BEGIN
-FOR fname IN SELECT oid::regprocedure FROM pg_catalog.pg_proc WHERE proname = 'delete_product' LOOP
+FOR fname IN SELECT oid::regprocedure FROM pg_catalog.pg_proc WHERE proname = 'delete_product_registration' LOOP
   EXECUTE 'DROP FUNCTION ' || fname;
 END loop;
 RAISE INFO 'FUNCTION % DROPPED', fname;
 END$$;
 -- Start function
-CREATE FUNCTION delete_product(
-        pProductId varchar(32)
+CREATE FUNCTION delete_product_registration(
+        pProductRegistrationId varchar(32)
 )
-RETURNS BOOLEAN AS 
+RETURNS BOOLEAN AS
 $BODY$
 BEGIN
-    IF pProductId IS NULL THEN  
+    IF pProductRegistrationId IS NULL THEN
         RETURN FALSE;
     ELSE
-        DELETE from product where 
-        product_id = pProductId;
+        DELETE from product_registration where
+        product_registration_id = pProductRegistrationId;
         RETURN TRUE;
     END IF;
 END;
