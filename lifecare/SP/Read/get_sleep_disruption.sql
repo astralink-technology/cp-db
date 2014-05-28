ed6568fa7321d5fbf396b537d691e0cc9d9df64f
Fong Shi Wei <shiweifong@gmail.com>
2014-05-28 14:42:06 +0800

    new analytics

--- lifecare/SP/Read/get_sleep_disruption.sql
+++ lifecare/SP/Read/get_sleep_disruption.sql
@@ -66,7 +66,7 @@ BEGIN
                    lead(e.create_date) over (ORDER BY eyecare_id) AS next_create_date
            FROM eyecare e WHERE
             ((pDeviceId = NULL) OR (e.device_id = pDeviceId))  AND
-            e.create_date BETWEEN pSleepTime + INTERVAL '1 minute' AND pWakeupTime AND((
+            e.create_date BETWEEN pSleepTime + INTERVAL '1 minute' AND pWakeupTime - INTERVAL '1 minute' AND((
                 (e.node_name = 'Door sensor' AND e.event_type_id = '20001' AND e.extra_data IN ('Alarm On', 'Alarm Off')) OR -- door sensor alarm report on door open "Alarm On"
                 (e.event_type_id IN ('20002', '20003', '20004') AND e.zone = 'Master Bedroom') OR -- Bedroom motion sensor alarm on
                 (e.event_type_id IN ('20002', '20003', '20004') AND e.zone = 'Kitchen') OR -- Kitchen  motion sensor alarm on
